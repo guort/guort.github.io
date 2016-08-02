f5a4b30de34916ae04f3fcd1eb2a4f955aa2d655
guoruitao <guoruitao@tvmining.com>
2016-08-02 14:08:31 +0800

    逻辑更改    后台更新数据， 逐条获取数据

--- danmuLeft.js
+++ danmuLeft.js
@@ -1,110 +1,117 @@
 
-// 弹幕功能插件
-    //传入的json格式
-    /* var Json = {
-             "name": "danmuData",
-             "target": "danmuTarget",
-             "data": [{'name': 'one','head': './images/tx.jpg','des': '恭喜你中奖了','img': './images/che.png','speed': '6'}
-                , {'name': 'two', 'head': './images/tx2.jpg', 'des': '描述...', 'img': './images/hua.png'}
-                , {'name': 'three', 'head': './images/tx.jpg', 'des': '恭喜你中奖了', 'img': './images/paoche.png'}
-                , {'name': 'four', 'head': './images/tx2.jpg', 'des': '描述...', 'img': './images/che.png','speed':'3'}
-                , {'name': 'five', 'head': './images/tx.jpg', 'des': '恭喜你中奖了', 'img': './images/hua.png'}
-                , {'name': 'six','head': './images/tx2.jpg','des': '描述...','img': './images/paoche.png','speed': '3'}]
-            ,"line":"1"
-            };
-*/
+window.onload = function () {
 
-function danmuAJAX() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('get','ajax1.php');
-    xhr.send(null);
-    xhr.onreadystatechange = function () {
-        if(xhr.readyState == 4 && xhr.status == 200){
-            var json = JSON.parse(xhr.responseText);
-            if(json.data[0]){
-                danmuLeft(json);
-            }else{
-                setTimeout(danmuAJAX,2000);
-            }
-        }
+    var Json = {
+        "target": "danmuTarget",
+        "data": [{'name': 'one', 'head': './images/tx.jpg', 'des': '恭喜你中奖了', 'img': './images/che.png', 'speed': '6'}
+            , {'name': 'two', 'head': './images/tx2.jpg', 'des': '描述...', 'img': './images/hua.png'}
+            , {'name': 'three', 'head': './images/tx.jpg', 'des': '恭喜你中奖了', 'img': './images/paoche.png'}
+            , {'name': 'four', 'head': './images/tx2.jpg', 'des': '描述...', 'img': './images/che.png', 'speed': '3'}
+            , {'name': 'five', 'head': './images/tx.jpg', 'des': '恭喜你中奖了', 'img': './images/hua.png'}
+            , {'name': 'six', 'head': './images/tx2.jpg', 'des': '描述...', 'img': './images/paoche.png', 'speed': '3'}]
+        // , "line": "1"
     };
-}
 
-function danmuLeft(Json) {
-    var data = Json.data,
-        line = Json.line,
-        tatgetId = Json.target || danmuTarget,
-        targetDom = document.getElementById(tatgetId),
-        danmu = document.createElement("div"),
-        timer = null,
-        danmuHeight = 0,
-        danmuWidth = 0,
-        dataLen = data.length,
-        speedArr = [],
-        speed,
-        size,
-        sizeArr = [],
-        mathWidth = 0,
-        mathHeight = 0,
-        html = "",
-        num = 0,
-        domList = [],
-        domH = 0,
-        domHCZ = 0,
-        domListLength = 0;
+    var btn = document.getElementById("btn");
 
-
-    danmu.className = "danmu";
-    targetDom.style.position = "relative";
-    targetDom.appendChild(danmu);
-    danmuWidth = danmu.offsetWidth;
-    danmuHeight = danmu.offsetHeight;
-
-    for (var i = 0; i < dataLen; i++) {
-        speed = data[i].speed || 5;
-        speedArr.push(speed);
-        size = data[i].size || 0.5;
-        sizeArr.push(size);
-        html += "<div class='flyLeft'><div class='head'>";
-        html += "<img src=\"" + data[i].head + "\"></div>";
-        html += "<span class='description'>" + data[i].des + "</span>";
-        html += "<div class=\"d_img\" style=\"background-image\: url("+ data[i].img +");\"></div>";
-        html += "</div>";
-    }
-    danmu.innerHTML += html;
-
-    domList = danmu.querySelectorAll(".flyLeft");
-    domListLength = domList.length;
-
-    for (var j = 0; j < domListLength; j++) {
-        domList[j].index = j;
-        //domList[j].style.webkitTransform ="scale("+ sizeArr[j] +")";
-        //3d转换，手机上运行比较流畅
-        domList[j].style.webkitTransformOrigin = "0 0 0";
-        domList[j].style.webkitTransform = "scale3d("+ sizeArr[j]+","+  sizeArr[j]+","+"1"+")";
-        domList[j].style.left = "100%";
-        domH = domList[j].offsetHeight*sizeArr[j];
-        if (line == 1) {
-            mathHeight = danmuHeight * 0.2;
-        } else {
-            mathHeight = Math.floor(Math.random() * (danmuHeight-domH));
-        }
-        domList[j].style.top = mathHeight + "px";
-        domList[j].style.webkitTransition = "left " + speedArr[j] + "s linear";
-        domList[j].addEventListener("webkitTransitionEnd",function () {
-            if(this.index == domListLength-1){
-                targetDom.removeChild(danmu);
-            }
-            danmu.removeChild(this);
+    btn.onclick = function () {
+        Json.data.push({
+            'name': 'six',
+            'head': './images/tx2.jpg',
+            'des': '描述...',
+            'img': './images/paoche.png',
+            'speed': '5'
         });
-    }
-    clearInterval(timer);
-    timer = setInterval(function () {
-        domList[num].style.left = "-300px";
-        num++;
-        if (num == domListLength) {
-            danmuAJAX();
-            clearInterval(timer);
+    };
+
+    startDanmu(Json);
+
+    function startDanmu(Json, time) {
+
+        var dTime = time || 1500, /*获取数据的时间间隔*/
+            line = Json.line, /*行数*/
+            tatgetId = Json.target || "danmuTarget", /*弹幕所在div的id*/
+            targetDom = document.getElementById(tatgetId), /*弹幕所在的dom节点*/
+            danmu = document.createElement("div"),
+            timer = null;
+
+        danmu.className = "danmu";
+        targetDom.appendChild(danmu);
+        targetDom.style.position = "relative";
+
+        clearInterval(timer);
+        // 不间断的获取数据
+        timer = setInterval(function () {
+            if (Json.data[0]) {
+                danmuLeft(Json);
+            }
+        }, dTime);
+
+        function danmuLeft(Json) {
+
+            //console.log(Json.data[0]);
+
+            var data = Json.data[0],
+                danmuWidth,
+                danmuHeight,
+                html = "",
+                speed = data.speed || 5,
+                size = data.size || 1,
+                mathHeight,
+                dom,
+                domP = document.createElement("div"),
+                domH,
+                domW;
+            domP.className = "dmFly";
+            danmuWidth = danmu.offsetWidth;
+            danmuHeight = danmu.offsetHeight;
+            //alert(1);
+
+            html += "<div class='flyLeft'><div class='head'>";
+            html += "<img src=\"" + data.head + "\"></div>";
+            html += "<span class='description'>" + data.des + "</span>";
+            html += "<div class=\"d_img\" style=\"background-image\: url(" + data.img + ");\"></div>";
+            html += "</div>";
+
+            domP.innerHTML = html;
+
+            danmu.appendChild(domP);
+            //console.log(danmu);
+
+            // 获取最新添加的dom节点
+            dom = danmu.lastElementChild;
+            //console.log(dom);
+            //设置缩放原点
+            dom.style.webkitTransformOrigin = "0 0 0";
+            dom.style.webkitTransform = "scale3d(" + size + "," + size + "," + "1" + ")";
+            //获取节点宽高
+            domH = dom.offsetHeight * size;
+            domW = dom.offsetWidth * size;
+            //设置定位高度
+            if (line == 1) {
+                mathHeight = danmuHeight * 0.2;
+            } else {
+                mathHeight = Math.floor(Math.random() * (danmuHeight - domH));
+            }
+            dom.style.top = mathHeight + "px";
+
+            dom.style.webkitTransition = "-webkit-transform " + speed + "s linear";
+            dom.style.webkitTransform = "translate3d(-" + (danmuWidth + domW) + "px," + 0 + "," + 0 + ")";
+            //dom.style.webkitTransform = "translateX(-" + (danmuWidth + domW) + "px)";
+            //dom.style.left = "-200px";
+            //animate(dom,-domW,speed*1000);
+
+            dom.addEventListener("webkitTransitionEnd", function () {
+                danmu.removeChild(this);
+            });
+            //删除数组中第一个元素
+            Json.data.shift();
+
         }
-    }, 1500);
-}
+
+    }
+
+};
+
+
+
